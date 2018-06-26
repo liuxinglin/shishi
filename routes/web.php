f225064ba5b3eab49edb3be4ba3f9bb6af501bf7
liuxinglin <183393021@qq.com>
2018-06-26 22:11:23 +0800

    路由文件提交

--- routes/web.php
+++ routes/web.php
@@ -29,14 +29,16 @@ Route::group(['namespace' => 'Backend', 'prefix' => 'backend'], function () {
 });
 
 Route::group(['namespace' => 'Home'], function () {
-    //登录注册
-    Route::get('/register', ['as' => 'game.to_reg', 'uses' => 'UserController@toReg']);
-    Route::post('/register', ['as' => 'game.to_reg', 'uses' => 'UserController@add']);
-    Route::get('/modifyPwd', ['as' => 'game.modify_pwd', 'uses' => 'UserController@toModifyPwd']);
-    Route::post('/modifyPwd', ['as' => 'game.modify_pwd', 'uses' => 'UserController@modifyPwd']);
+    Route::get('/members/index', 'MemberController@index');
+    Route::post('/members/bindPhone', 'MemberController@bindPhone');
     Route::get('/product/index', 'ProductController@index');
-    Route::get('/products/tryout', 'TryoutProductController@index');
+    Route::get('/tryoutProducts/details', 'TryoutProductController@show');
+    Route::get('/tryoutProducts/list', 'TryoutProductController@index');
     Route::resource('products', 'ProductController');
+    Route::resource('enrolments', 'EnrolmentController');
+    Route::post('/enrolments/add', 'EnrolmentController@store');
+    Route::get('/enrolments/details', 'EnrolmentController@show');
+    Route::post('/votes/add', 'VoteController@store');
 });
 
 
