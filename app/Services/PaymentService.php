70c91ed88dd09358a7ec95b524fc6480e9c431a4
liuxinglin <183393021@qq.com>
2018-08-19 20:04:39 +0800

    新增支付回调

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -25,7 +25,7 @@ class PaymentService
             'out_trade_no' => $orderInfo['order_id'],
             'total_fee' => $orderInfo['total'],
             //'spbill_create_ip' => '123.12.12.123', // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
-            //'notify_url' => '', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
+            'notify_url' => 'http://www.52mhf.com/payments/notify', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
             'trade_type' => 'JSAPI',
             'openid' => $orderInfo['openid'],
         ]);
